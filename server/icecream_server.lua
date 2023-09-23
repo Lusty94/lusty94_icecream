db11e4c84c54c09eafd36fd5d3b0d9d0d23c3ed4
Lusty94 <108025667+Lusty94@users.noreply.github.com>
2023-09-23 19:07:40 +0100

    added version check

--- server/icecream_server.lua
+++ server/icecream_server.lua
@@ -935,3 +935,16 @@ else
 end
 
 
+
+
+local function CheckVersion()
+	PerformHttpRequest('https://raw.githubusercontent.com/Lusty94/UpdatedVersions/main/IceCream/version.txt', function(err, newestVersion, headers)
+		local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+		if not newestVersion then print("Currently unable to run a version check.") return end
+		local advice = "^1You are currently running an outdated version^7, ^1please update^7"
+		if newestVersion:gsub("%s+", "") == currentVersion:gsub("%s+", "") then advice = '^6You are running the latest version.^7'
+		else print("^3Version Check^7: ^5Current^7: "..currentVersion.." ^5Latest^7: "..newestVersion.." "..advice) end
+	end)
+end
+CheckVersion()
+
